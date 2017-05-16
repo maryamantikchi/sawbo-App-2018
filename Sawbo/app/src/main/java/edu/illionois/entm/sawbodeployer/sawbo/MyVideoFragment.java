d8f53d5aa2fef0fa7705c30f6474d6cfb56eceaf
Mahsa <mahsad71@gmail.com>
2017-05-17 00:44:15 +0430

    about_string

--- Sawbo/app/src/main/java/edu/illionois/entm/sawbodeployer/sawbo/MyVideoFragment.java
+++
@@ -1,9 +0,0 @@
-package edu.illionois.entm.sawbodeployer.sawbo;
-
-/**
- * Created by Mahsa on 4/1/2017.
- */
-public class MyVideoFragment extends android.support.v4.app.Fragment {
-    public MyVideoFragment(){
-        }
-}
